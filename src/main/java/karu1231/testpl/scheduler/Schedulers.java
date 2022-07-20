6530c0d578ce0e73725ef43588f3b642c887d01e
karu1231 <51406692+karu1231@users.noreply.github.com>
2022-07-20 19:28:16 +0900

    [change] schedulerの関数返り値をvoid->taskIDに変更

--- src/main/java/karu1231/testpl/scheduler/Schedulers.java
+++ src/main/java/karu1231/testpl/scheduler/Schedulers.java
@@ -4,6 +4,7 @@ import karu1231.testpl.privateGit.TestPL;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.scheduler.BukkitScheduler;
+import org.bukkit.scheduler.BukkitTask;
 
 public class Schedulers {
 
@@ -11,9 +12,9 @@ public class Schedulers {
 	private static final Plugin plugin = TestPL.plugin;
 
 	/* タスクが作られたすぐ次のtickで起動。 同期に使う。 */
-	public static void runTask() {
+	public static int runTask() {
 
-		scheduler.runTask(plugin, new Runnable() {
+		return scheduler.runTask(plugin, new Runnable() {
 
 			@Override
 			public void run() {
@@ -22,17 +23,17 @@ public class Schedulers {
 
 			}
 
-		});
+		}).getTaskId();
 
 	}
 
 	/* タスクが作られてから一定時間置かれたtickで起動。 */
-	public static void delayTask() {
+	public static int delayTask() {
 
 		/* 遅らせる時間を引数としてもたせる。 設定数値の単位はtickで1秒 = 20tick */
 		long delaytick = 20L;
 
-		scheduler.scheduleSyncDelayedTask(plugin, new Runnable() {
+		return scheduler.scheduleSyncDelayedTask(plugin, new Runnable() {
 
 			@Override
 			public void run() {
@@ -46,7 +47,7 @@ public class Schedulers {
 	}
 
 	/* タスクが作られてから止められるまで一生設定した時間ごとに実行される。 */
-	public static void repeatTask() {
+	public static int repeatTask() {
 
 		/* リピートごとの遅延時間とタスクが作られてから一番最初のリピートの遅延時間をを引数としてもたせる。
 		 * 最初のリピートの遅延時間を0にすると、タスクが作られた次のtickで実行される。
@@ -54,7 +55,7 @@ public class Schedulers {
 		long firstdelay = 0L;
 		long repeattick = 10L;
 
-		scheduler.scheduleSyncRepeatingTask(plugin, new Runnable() {
+		return scheduler.scheduleSyncRepeatingTask(plugin, new Runnable() {
 
 			@Override
 			public void run() {
